40c7fdcb5a671f1c7432f7ebd2d367927b0c31e0
laura.tombs@gmail.com <laura.tombs@gmail.com>
2016-10-31 09:20:10 +0000

    adding to git

---
+++ src/components/app.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import CreateTodo from './create-todo';
+import TodosList from './todos-list';
+
+const todos = [
+  {
+    task: "make React tutorial",
+    isCompleted:  true
+  },
+  {
+    task: "eat dinner",
+    isCompleted:  false
+  },
+];
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos
+    };
+  }
+  render() {
+    return (
+      <div>
+      <CreateTodo todos={this.state.todos} createTask={this.createTask.bind(this)} />
+      <TodosList 
+          todos     ={this.state.todos}
+          toggleTask={this.toggleTask.bind(this)}
+          saveTask  ={this.saveTask.bind(this)}
+          deleteTask={this.deleteTask.bind(this)}
+          />
+      </div>
+    );
+  }
+
+  toggleTask(task) {
+    const foundToDo = _.find(this.state.todos, todo => todo.task === task);
+    foundToDo.isCompleted = !foundToDo.isCompleted;
+    this.setState({ todos: this.state.todos });
+  }
+
+  createTask(task){
+    this.state.todos.push({
+      task,
+      isCompleted: false
+    });
+    this.setState({ todos: this.state.todos })
+  }
+
+  saveTask(oldTask, newTask) {
+    const foundToDo = _.find(this.state.todos, todo => todo.task === oldTask);
+    foundToDo.task  = newTask;
+    this.setState({ todos: this.state.todos });
+  }
+
+  deleteTask(taskToDelete) {
+    _.remove(this.state.todos, todo => todo.task === taskToDelete);
+    this.setState({ todos:this.state.todos});
+  }
+}
